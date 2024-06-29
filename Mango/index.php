9ee1742432990366e9d02470d84b040ce3678e28
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-06-29 21:02:36 +0800

    Add files via upload

---
+++ Mango/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 移植自 WoerdPress 的 Mango 主题，原作者：疯狂的大叔。
+ *
+ * @package Mango For Typecho
+ * @author 大哲
+ * @version Dev 1.0
+ * @link https://zhebk.cn
+ */
+
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+$this->need('header.php');
+?>
