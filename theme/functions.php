caee3690917eeed67207965e87a6e95d59cfd8c8
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-06-29 21:02:56 +0800

    Delete theme/functions.php

--- theme/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function themeConfig($form) {
-
-  $subTitle  = new Typecho_Widget_Helper_Form_Element_Text('subTitle', NULL,'个人博客', _t('网站副标题'), _t('默认内容"个人博客"'));
-  $form->addInput($subTitle);
-
-}
