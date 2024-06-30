496c7ac95d5262efe917d335d5523888730f0070
Zisbusy <Zisbusy@outlook.com>
2024-06-30 22:32:09 +0800

    添加头部

--- Mango/header.php
+++ Mango/header.php
@@ -36,7 +36,71 @@
     }
 ?></title>
 <?php $this->header('generator=&template=&pingback=&xmlrpc=&wlw=&rss2=&rss1=&atom=&commentReply=&antiSpam='); ?>
+<link rel="shortcut icon" href="/usr/themes/Mango/assets/img/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/css/bootstrap.min.css'); ?>" type="text/css" media="all">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/css/fancybox.css'); ?>" type="text/css" media="all">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/bifont/bootstrap-icons.css'); ?>" type="text/css" media="all">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/css/style.css'); ?>" type="text/css" media="all">
+<script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/jquery.min.js'); ?>" id="jquery-min-js"></script>
+</head>
 
-<link rel="stylesheet" href="<?php $this->options->themeUrl('normalize.css'); ?>">
-<link rel="stylesheet" href="<?php $this->options->themeUrl('grid.css'); ?>">
-<link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
+<body class="home blog">
+  <style>:root{--ds_background:url(/usr/themes/Mango/assets/img/background.svg)}</style>
+  <header class="header sticky-top">
+    <div class="container">
+      <div class="top">
+        <!-- 手机端菜单按钮 -->
+        <button class="mobile_an" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobile_right_nav"><i class="bi bi-list"></i></button>
+        <!-- PC端左侧-->
+        <div class="top_l">
+          <h1 class="logo">
+            <a href="/" title="<?php $this->options->title() ?>">
+              <img src="/usr/themes/Mango/assets/img/logo.png" alt="">
+              <b><?php $this->options->title() ?></b>
+            </a>
+          </h1>
+          <?php $this->need('/inc/nav.php'); ?>
+        </div>
+        <!-- PC端右侧-->
+        <div class="top_r">
+            <div class="top_r_an theme-switch me-4" onclick="switchDarkMode()"><i class="bi bi-lightbulb-fill"></i></div>
+            <button class="top_r_an" type="button" data-bs-toggle="offcanvas" data-bs-target="#c_sousuo"><i class="bi bi-search"></i></button>
+        </div>
+      </div>
+    </div>
+  </header>
+
+
+
+
+  <section class="links mobile_none">
+    <div class="container">
+        <span>友情链接：</span>
+        <a href="http://www.2zzt.com" rel="noopener" target="_blank">WordPress</a>
+<a href="https://www.huitheme.com" rel="noopener" title="wordpress" target="_blank">wordpress</a>
+<a href="http://www.2zzt.com" rel="noopener" target="_blank">WordPress Theme</a>
+<a href="https://www.huitheme.com" rel="noopener" title="WordPress企业主题" target="_blank">WordPress企业主题</a>
+<a href="https://www.huitheme.com" rel="noopener" title="WordPress插件" target="_blank">WordPress插件</a>
+<a href="https://www.huitheme.com" rel="noopener" title="WordPress教程" target="_blank">WordPress教程</a>
+<a href="https://www.huitheme.com" rel="noopener" target="_blank">WordPress模版</a>
+<a href="https://www.huitheme.com" rel="noopener" title="绘主题" target="_blank">绘主题</a>
+    </div>
+</section>
+
+<footer class="footbox">
+    <div class="container">
+    	<div class="copyright">
+    		<p>©️ 2022 Mango All Rights Reserved. Powered by <a href="https://www.huitheme.com/" target="_blank">WordPress</a>
+	    						<a class="beian" href="https://beian.miit.gov.cn/" rel="external nofollow" target="_blank" title="备案号"><i class="bi bi-shield-check me-1"></i>苏ICP备123456号</a>
+					    	</p>
+    	</div>
+	</div>
+</footer>
+
+<button class="scrollToTopBtn" title="返回顶部"><i class="bi bi-chevron-up"></i></button>
+
+
+<script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/bootstrap.min.js'); ?>" id="bootstrap-js"></script>
+<script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/fancybox.js'); ?>" id="fancybox-js"></script>
+<script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/js.js'); ?>" id="dsjs-js"></script>
+</body>
---
+++ Mango/inc/nav.php
@@ -0,0 +1,36 @@
+<nav class="header-menu">
+  <ul class="header-menu-ul">
+    <li><a href="/">首页</a></li>
+    <!-- 输出文章分类 -->
+    <?php $this->widget('Widget_Metas_Category_List')->to($categorys); ?>
+    <?php while($categorys->next()): ?>
+      <?php if ($categorys->levels === 0): ?>
+        <?php $children = $categorys->getAllChildren($categorys->mid); ?>
+        <?php if (empty($children)) { ?>
+          <!-- 遍历一级标题 -->
+          <li>
+            <a href="<?php $categorys->permalink(); ?>" title="<?php $categorys->name(); ?>"><?php $categorys->name(); ?></a>
+          </li>
+        <?php } else { ?>
+          <li class="menu-item-has-children">
+            <a href="<?php $categorys->permalink(); ?>" title="<?php $categorys->name(); ?>"><?php $categorys->name(); ?></a>
+            <!-- 二级标题 -->
+            <ul class="sub-menu">
+              <?php foreach ($children as $mid) { ?>
+              <?php $child = $categorys->getCategory($mid); ?>
+                <li>
+                  <a href="<?php echo $child['permalink'] ?>" title="<?php echo $child['name']; ?>"><?php echo $child['name']; ?></a>
+                </li>
+              <?php } ?>
+            </ul>
+          </li>
+        <?php } ?>
+      <?php endif; ?>
+    <?php endwhile; ?>
+    <!-- 输出独立页面 -->
+    <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+    <?php while($pages->next()): ?>
+      <li><a href="<?php $pages->permalink(); ?>"><?php $pages->title(); ?></a></li>
+    <?php endwhile; ?>
+  </ul>
+</nav>
