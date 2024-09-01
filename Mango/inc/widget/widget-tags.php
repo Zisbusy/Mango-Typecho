8a51159b9f0dfb629f141f7228952d2d6e705f9b
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-09-01 18:00:09 +0800

    Update widget-tags.php

--- Mango/inc/widget/widget-tags.php
+++ Mango/inc/widget/widget-tags.php
@@ -1,4 +1,4 @@
-<aside id="hot_tags-3" class="widget widget_hot_tags">
+<aside class="widget widget_hot_tags">
   <h3 class="widget-title">热门标签</h3>
   <div class="tagcloud">
     <?php \Widget\Metas\Tag\Cloud::alloc('ignoreZeroCount=1&desc=1&limit=15')->to($tags); ?>
