ef1a0f34756874fa2607648ff5729a0bc6f12188
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-10-18 21:35:32 +0800

    Update widget-hotpost.php

--- Mango/inc/widget/widget-hotpost.php
+++ Mango/inc/widget/widget-hotpost.php
@@ -1,24 +1,4 @@
-<?php
-class Widget_Contents_Post_Comments extends Widget_Abstract_Contents{
-  public function execute(){
-    $this->parameter->setDefault(array('pageSize' => $this->options->postsListSize));
-    $this->db->fetchAll($this->select()
-    ->where('table.contents.status = ?', 'publish')
-    ->where('table.contents.created <= ?', $this->options->time)
-    ->where('table.contents.type = ?', 'post')
-    ->order('commentsNum', Typecho_Db::SORT_DESC)
-    ->limit($this->parameter->pageSize), array($this, 'push'));
-  }
-}
-
-function getFirstImageContent($html) {
-  $pattern = '/<img[^>]+src=[\'"]([^\'"]+)[\'"][^>]*>/i';
-  if (preg_match($pattern, $html, $matches)) {
-    return $matches[1];
-  }
-  return "/usr/themes/Mango/assets/img/hostpostimg.webp";
-}
-?>
+<?php $this->need('/inc/hotpost.php'); ?>
 
 <aside class="widget widget_hot_posts">
   <h3 class="widget-title">热门文章</h3>
