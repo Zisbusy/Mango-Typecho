228cc265cb72ffac68eb5706a4636faaa8059531
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-11-05 17:36:26 +0800

    Update tag.php

--- Mango/inc/tag.php
+++ Mango/inc/tag.php
@@ -1,7 +1,9 @@
+<?php if (count($this->tags) > 0): ?>
 <div class="post_loop_tag">
-  <?php if (count($this->tags) > 0): ?>
-    <em>
-      <i class="bi bi-hash"></i><?php $this->tags ('</em><em><i class="bi bi-hash"></i>', true, 'none'); ?>
-    </em>
-  <?php endif; ?>
+  <em>
+    <i class="bi bi-hash"></i><?php $this->tags ('</em><em><i class="bi bi-hash"></i>', true, 'none'); ?>
+  </em>
 </div>
+<?php endif; ?>
+
+
