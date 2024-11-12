5e36b020ae6befb34c97a02f244e1322506c2142
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-11-12 13:30:44 +0800

    Update index.php

--- Mango/index.php
+++ Mango/index.php
@@ -15,26 +15,35 @@ $this->need('header.php');
 <?php if($this->is('index')){$this->need('inc/banner.php');} ?>
 <!-- 内容展示 -->
 <section class="index_area">
-    <div class="container">
-        <div class="row g-4">
-            <div class="col-lg-8">
-              <!-- 文章列表 -->
-              <?php $this->need('inc/excerpt.php'); ?>
-              <!-- 文章翻页 -->
-              <?php $this->need('inc/flip.php'); ?>
-            </div>
-            <div class="col-lg-4">
-              <div class="sidebar_sticky">
-                <!-- 热门文章 -->
-                <?php $this->need('inc/widget/widget-hotpost.php'); ?>
-                <!-- 热门标签 -->
-                <?php $this->need('inc/widget/widget-tags.php'); ?>
-                <!-- 最近评论 -->
-                <?php $this->need('inc/widget/widget-comments.php'); ?>
-              </div>
-            </div>
+  <div class="container">
+    <div class="row g-4">
+      <div class="col-lg-8">
+        <?php if($this->is('index')):?>
+          <!-- 文章列表 -->
+          <?php $this->need('inc/excerpt.php'); ?>
+          <!-- 文章翻页 -->
+          <?php $this->need('inc/flip.php'); ?>
+        <?php else: ?>    
+          <div class="catbox">
+          <!-- 文章列表 -->
+          <?php $this->need('inc/excerpt.php'); ?>
+          <!-- 文章翻页 -->
+          <?php $this->need('inc/flip.php'); ?>
+          </div>
+        <?php endif; ?>
+      </div>
+      <div class="col-lg-4">
+        <div class="sidebar_sticky">
+        <!-- 热门文章 -->
+        <?php $this->need('inc/widget/widget-hotpost.php'); ?>
+        <!-- 热门标签 -->
+        <?php $this->need('inc/widget/widget-tags.php'); ?>
+        <!-- 最近评论 -->
+        <?php $this->need('inc/widget/widget-comments.php'); ?>
         </div>
+      </div>
     </div>
+  </div>
 </section>
 <!-- 页底信息 -->
 <?php $this->need('footer.php'); ?>
