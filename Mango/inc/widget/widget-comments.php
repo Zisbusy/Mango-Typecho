713df142dadfb5235ac1d70a23f2274b447b4dab
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-11-23 13:01:23 +0800

    Update widget-comments.php

--- Mango/inc/widget/widget-comments.php
+++ Mango/inc/widget/widget-comments.php
@@ -1,15 +1,3 @@
-<?php
-function getavatar($email) {
-  //判断邮箱类型选取头像地址
-  if(preg_match('|^[1-9]\d{4,10}@qq\.com$|i',$email)){
-    $avatar = '//q.qlogo.cn/g?b=qq&nk=' . explode("@",$email)[0]. '&s=160';
-  }else{
-    $avatar = 'https://gravatar.loli.net/avatar/' . md5(strtolower($email)) . '?s=160&r=X&d=mm';
-  }
-  return $avatar;
-}
-?>
-
 <aside class="widget widget_comments">
   <h3 class="widget-title">最近评论</h3>
   <ul class="widget_comment_ul">
