a521523ebfa4e910b923344f98de352a1aace263
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-11-29 20:59:20 +0800

    Update avatar.php

--- Mango/inc/core/avatar.php
+++ Mango/inc/core/avatar.php
@@ -1,11 +1,14 @@
 <?php
 function getavatar($email) {
+  // 空邮箱返回默认头像
+  if($email == '') {
+    return 'https://gravatar.loli.net/avatar/c7cfa13aedff7a445ffd7bc98eb33272?s=80&d=mm&r=g';
+  }
   //判断邮箱类型选取头像地址
   if(preg_match('|^[1-9]\d{4,10}@qq\.com$|i',$email)){
-    $avatar = '//q.qlogo.cn/g?b=qq&nk=' . explode("@",$email)[0]. '&s=160';
+    return '//q.qlogo.cn/g?b=qq&nk=' . explode("@",$email)[0]. '&s=160';
   }else{
-    $avatar = 'https://gravatar.loli.net/avatar/' . md5(strtolower($email)) . '?s=160&r=X&d=mm';
+    return 'https://gravatar.loli.net/avatar/' . md5(strtolower($email)) . '?s=160&r=X&d=mm';
   }
-  return $avatar;
 }
 ?>
