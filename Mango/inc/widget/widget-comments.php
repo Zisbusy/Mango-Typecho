4969d4a78f7abe78b6810fa2b1688f983e595372
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-12-31 16:22:44 +0800

    Update widget-comments.php

--- Mango/inc/widget/widget-comments.php
+++ Mango/inc/widget/widget-comments.php
@@ -1,7 +1,7 @@
 <aside class="widget widget_comments">
   <h3 class="widget-title">最近评论</h3>
   <ul class="widget_comment_ul">
-    <?php $this->widget('Widget_Comments_Recent','pageSize=10&ignoreAuthor=true')->to($recent);?>
+    <?php $this->widget('Widget_Comments_Recent','pageSize=5&ignoreAuthor=true')->to($recent);?>
     <?php while($recent->next()):?>
     <li>
       <img alt="" src="<?php echo getavatar($recent->mail); ?>" class="avatar avatar-40 photo" height="40" width="40" loading="lazy" decoding="async">
