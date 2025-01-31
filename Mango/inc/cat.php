0e634806ad1699ea4bcc96d2fad53af11c968e9c
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2025-01-31 16:20:44 +0800

    Update cat.php

--- Mango/inc/cat.php
+++ Mango/inc/cat.php
@@ -13,38 +13,31 @@ function getMid($pageRow) {
 }
 
 // 根据分类 mid 获取对应 mid 的图片
+// 根据分类 mid 获取对应 mid 的图片
 function findImageByMid($mid) {
   // 定义 category 文件夹的路径
   $categoryPath = '/usr/themes/Mango/assets/img/category/';
   $localcategoryPath = __TYPECHO_ROOT_DIR__ . $categoryPath;
-    
-  // 获取 Typecho 配置项目
-  $options = Typecho_Widget::widget('Widget_Options');
-  // 获取后台博客域名
-  if (substr($options->siteUrl, -1) == '/') {
-    // 移除最后一个字符（斜杠）
-    $siteUrl = substr($options->siteUrl, 0, -1);
-  }
   
-  // 如果传入为空，返回默认图片
-  if (empty($mid)) {
-    return $siteUrl . '/usr/themes/Mango/assets/img/category/default.webp';
-  }
-
-  // 获取 category 文件夹中的所有文件
-  $files = scandir($localcategoryPath);
-
-  // 遍历文件列表，寻找与 mid 匹配的图片文件
-  foreach ($files as $file) {
-    // 去掉文件扩展名
-    $baseName = pathinfo($file, PATHINFO_FILENAME);
-    // 检查文件名是否匹配 mid
-    if ($baseName == $mid) {
-      // 如果找到了匹配的文件，返回文件的完整路径
-      return $siteUrl . $categoryPath . $file;
+  // 获取 Typecho 域名
+  $siteUrl = Typecho_Widget::widget('Widget_Options')->rootUrl;
+  
+  // 如果传入不为空，返回设置图片
+  if (!empty($mid)) {
+    // 获取 category 文件夹中的所有文件
+    $files = scandir($localcategoryPath);
+    // 遍历文件列表，寻找与 mid 匹配的图片文件
+    foreach ($files as $file) {
+      // 去掉文件扩展名
+      $baseName = pathinfo($file, PATHINFO_FILENAME);
+      // 检查文件名是否匹配 mid
+      if ($baseName == $mid) {
+        // 如果找到了匹配的文件，返回文件的完整路径
+        return $siteUrl . $categoryPath . $file;
+      }
     }
   }
-
+  
   // 如果没有找到匹配的文件，返回默认图片
   return $siteUrl . '/usr/themes/Mango/assets/img/category/default.webp';
 }
