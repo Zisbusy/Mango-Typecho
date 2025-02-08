ee3b3034ece2086625c6b0c684bb00b93dcefc2b
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2025-02-08 14:27:10 +0800

    Update views.php

--- Mango/inc/core/views.php
+++ Mango/inc/core/views.php
@@ -17,17 +17,25 @@ function Postviews($archive) {
   
   // 获取当前文章的浏览次数
   $exist = $db->fetchRow($db->select('views')->from('table.contents')->where('cid = ?', $cid))['views'];
+
   if ($archive->is('single')) {
-    $cookie = Typecho_Cookie::get('contents_views');
+    // 获取 Cookie 中的文章浏览记录
+    $cookie = isset($_COOKIE['Views']) ? $_COOKIE['Views'] : '';
     $cookie = $cookie ? explode(',', $cookie) : array();
+
+    // 如果当前文章没有被浏览过
     if (!in_array($cid, $cookie)) {
+      // 更新数据库中的浏览次数
       $db->query($db->update('table.contents')
-        ->rows(array('views' => (int)$exist+1))
-        ->where('cid = ?', $cid));
-      $exist = (int)$exist+1;
+         ->rows(array('views' => (int)$exist + 1))
+         ->where('cid = ?', $cid));
+      // 更新浏览次数变量
+      $exist = (int)$exist + 1;
+      // 将当前文章 ID 添加到 Cookie 中
       array_push($cookie, $cid);
       $cookie = implode(',', $cookie);
-      Typecho_Cookie::set('contents_views', $cookie);
+      // 设置 Cookie，有效期为 30 天
+      setcookie('Views', $cookie, time() + 2592000, '/');
     }
   }
   return $exist;
