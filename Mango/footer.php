4e6579b16625e0912f9825e78d1b779f4cb80653
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2025-02-09 17:25:27 +0800

    Update footer.php

--- Mango/footer.php
+++ Mango/footer.php
@@ -1,21 +1,17 @@
-
-
-
-
-
+<?php
+// 公安联网备案 获取备案号
+function WangAnNum($str) {
+  preg_match_all('/\d+/', $str, $matches);
+  $result = implode('', $matches[0]);
+  return $result;
+}
+?>
 
 <section class="links mobile_none">
-    <div class="container">
-        <span>友情链接：</span>
-        <a href="http://www.2zzt.com" rel="noopener" target="_blank">WordPress</a>
-<a href="https://www.huitheme.com" rel="noopener" title="wordpress" target="_blank">wordpress</a>
-<a href="http://www.2zzt.com" rel="noopener" target="_blank">WordPress Theme</a>
-<a href="https://www.huitheme.com" rel="noopener" title="WordPress企业主题" target="_blank">WordPress企业主题</a>
-<a href="https://www.huitheme.com" rel="noopener" title="WordPress插件" target="_blank">WordPress插件</a>
-<a href="https://www.huitheme.com" rel="noopener" title="WordPress教程" target="_blank">WordPress教程</a>
-<a href="https://www.huitheme.com" rel="noopener" target="_blank">WordPress模版</a>
-<a href="https://www.huitheme.com" rel="noopener" title="绘主题" target="_blank">绘主题</a>
-    </div>
+  <div class="container">
+    <span>友情链接：</span>
+    <a href="https://typecho.org/" rel="noopener" target="_blank">Typecho</a>
+  </div>
 </section>
 
 <footer class="footbox">
@@ -28,19 +24,20 @@
             <?php $this->options->ICP();?>
           </a>
         <?php endif; ?>
+        <?php if ($this->options->WangAn): ?>
+        <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=<?php echo WangAnNum($this->options->WangAn);?>" rel="nofollow noopener noreferrer"  target="_blank" title="公安备案号">
+            <i class="bi bi-shield-check me-1"></i>
+          <?php $this->options->WangAn();?>
+        </a>
+        <?php endif; ?>
       </p>
     </div>
   </div>
 </footer>
 
 <button class="scrollToTopBtn" title="返回顶部"><i class="bi bi-chevron-up"></i></button>
-
-
 <script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/bootstrap.min.js'); ?>" id="bootstrap-js"></script>
 <script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/fancybox.js'); ?>" id="fancybox-js"></script>
 <script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/js.js'); ?>" id="dsjs-js"></script>
 </body>
 </html>
-
-
-
