490a8a03ea3686c2899d504b1551e3bd9dbd442d
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2025-03-09 20:14:43 +0800

    Update functions.php

--- Mango/functions.php
+++ Mango/functions.php
@@ -54,8 +54,6 @@ function themeConfig($form) {
 
 // 修改后台设置以适应模板
 function themeInit($archive){
-  // 修改默认头像源
-  define('__TYPECHO_GRAVATAR_PREFIX__', 'https://weavatar.com/avatar/');
   // 将较新的的评论显示在前面
   Helper::options()->commentsOrder = 'DESC';
   // 启用评论分页
@@ -77,6 +75,8 @@ function themeInit($archive){
   }
 }
 
+// 修改默认头像源
+define('__TYPECHO_GRAVATAR_PREFIX__', 'https://weavatar.com/avatar/');
 // 图片压缩
 require_once('inc/core/thumb.php');
 // img 标签处理
