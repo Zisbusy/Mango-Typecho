651405722d6696a76d9a4f8573b63f5471617d6b
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2025-04-22 20:24:12 +0800

    Update flip.php

--- Mango/inc/flip.php
+++ Mango/inc/flip.php
@@ -2,16 +2,6 @@
   <div class="post-read-more">
     <?php $this->pageLink('加载更多','next'); ?>
   </div>
-  <script>
-    $('.post-read-more .next').on('click', function(event) {
-      // 阻止默认行为
-      event.preventDefault(); 
-      // 获取 href 属性
-      let href = $(this).attr('href'); 
-      // 修改地址栏链接
-      history.replaceState({}, document.title, href);
-    });
-  </script>
 <?php else: ?>
   <?php 
     $this->pageNav('上页', '下页', 1, '...', 
