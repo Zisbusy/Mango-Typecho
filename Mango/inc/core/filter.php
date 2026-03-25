f29438c470779865488e9239f8f2f89b43be043f
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2026-03-25 11:02:10 +0800

    Trim whitespace in contentFilter function

--- Mango/inc/core/filter.php
+++ Mango/inc/core/filter.php
@@ -1,9 +1,9 @@
 <?php
 // 过滤评论内容后友好提示。
 function contentFilter($content) {
-  $content= strip_tags($content);
+  $content = trim(strip_tags($content));
   if (empty($content)) {
-    return "违规评论（已过滤）";
+    return "违规（已过滤）";
   }
   return $content; 
 }
